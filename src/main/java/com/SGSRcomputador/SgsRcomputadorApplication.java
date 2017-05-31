249e185f73bcc2179163b239122e62cc4b6a2c68
carlos henrique da silva melo <carlossilvamelo7@hotmail.com>
2017-05-31 14:26:58 -0300

    estrutura do projeto

---
+++ src/main/java/com/SGSRcomputador/SgsRcomputadorApplication.java
@@ -0,0 +1,12 @@
+package com.SGSRcomputador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsRcomputadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsRcomputadorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/SGSRcomputador/SgsRcomputadorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SGSRcomputador;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SgsRcomputadorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
